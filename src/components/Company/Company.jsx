ea40e68cc1220a6288eb3d89e165f0f43653a40a
Maxim Kutprot <maksim.kutprot@daubit.org>
2023-05-10 21:41:12 +0200

    media info

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -34,6 +34,42 @@ const Company = () => {
           className={`absolute w-[900px] h-[300px]  ${styles.fotoShadow}`}
         ></div>
       </div>
+      <div className={styles.wrap2}>
+        <div className="w-[100%] flex justify-center text-center">
+          <div>
+            <h2 className="font-bold text-[#0097B2] pt-[25px]">Über uns</h2>
+            <p className=" w-[100%] pt-5 text-start px-7 pb-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga in
+              deleniti recusandae facilis explicabo rem illum, modi, eos, saepe
+              repellat natus laborum! Maxime praesentium est animi accusamus ex
+              eaque error. Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Fuga in deleniti recusandae facilis explicabo rem illum,
+              modi, eos, saepe repellat natus laborum! Maxime praesentium est
+              animi accusamus ex eaque error. Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Fuga in deleniti recusandae facilis
+              explicabo rem illum, modi, eos, saepe repellat natus laborum!
+              Maxime praesentium est animi accusamus ex eaque error. Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Fuga in
+              deleniti recusandae facilis explicabo rem illum, modi, eos, saepe
+              repellat natus laborum! Maxime praesentium est animi accusamus ex
+              eaque error.
+            </p>
+          </div>
+        </div>
+        <div className={`relative ${styles.block2}`}>
+          <div className="w-[100%] flex justify-center">
+            <div className={`absolute  ${styles.foto2}`}>
+              <Image
+                src="/images/company.png"
+                width={500}
+                height={500}
+                // fill={true}
+              />
+            </div>
+          </div>
+          <div className={`absolute  ${styles.fotoShadow2}`}></div>
+        </div>
+      </div>
     </div>
     // <div className="relative">
     //   <div className="w-[50%] h-[500px] bg-[#81AFC3]"></div>
