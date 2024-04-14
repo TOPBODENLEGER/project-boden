6ea3685d68b437ed45bd3487cc9c06ee872f3702
Maxim Kutprot <maksim.kutprot@daubit.org>
2024-04-14 17:36:59 +0200

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ slider }) {
   return (
     <>
       <Layout
-        title=""
+        title="Parkett schleifen Köln. Parkett verlegen. Parkett Ölen."
         description="Parkett schleifen. Verlegung neuer Böden (Parkett, Laminat, Vinyl), Professionelles Abschleifen von Parkett und Dielen Versiegelung mit umweltfreundlichem lacken auf Wasserbasis, Öl oder Hartwachsöl. Renovierung alter Holzböden (abschleifen, ölen, versiegeln), Restaurierung von Holztreppen. Köln, Bergisch Gladbach, Bonn, Düsseldorf"
       >
         <Banner />
